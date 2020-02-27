d4ab4450f5babc8fe9c95ab1ead36a861d229f8b
gokel166 <okellogeorge40@yahoo.com>
2020-02-27 10:26:15 -0600

    s

--- Ignite_Leadership/Startup.cs
+++ Ignite_Leadership/Startup.cs
@@ -28,7 +28,7 @@ namespace Ignite_Leadership
         public void ConfigureServices(IServiceCollection services)
         {
             //
-            //
+            //ss
             services.Configure<CookiePolicyOptions>(options =>
             {
                 // This lambda determines whether user consent for non-essential cookies is needed for a given request.
